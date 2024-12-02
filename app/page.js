87d12c3c2542b15a031d4ba9ffa81cd0cd4db18b
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-12-02 15:28:41 -0500

    on comp customization

--- app/page.js
+++ app/page.js
@@ -2,9 +2,10 @@ import ButtonLogin from "@/components/ButtonLogin";
 
 export default function Home() {
   const isLoggedIn = true;
-  const name = "ben";
+  const name = "Ben";
   return (
     <main>
+      {/* HEADER */}
       <section className="bg-base-200">
         <div className="flex justify-between items-center px-8 py-2 mx-auto max-w-3xl  ">
           <div className="font-bold">CodeFast SAAS</div>
@@ -17,7 +18,7 @@ export default function Home() {
           </div>
         </div>
       </section>
-
+      {/* HERO */}
       <section className="text-center py-32 px-8 max-w-3xl mx-auto">
         <h1 className="text-4xl lg:text-5xl font-extrabold mb-6">
           Collect customer feedback to build better products
@@ -31,6 +32,89 @@ export default function Home() {
         {/* <div>This is children</div> */}
         {/* </ButtonLogin> */}
       </section>
+      {/*  PRICING*/}
+      <section className="bg-base-200">
+        <div className="py-32 px-8 max-w-3xl mx-auto">
+          <p className="text-sm uppercase font-medium text-center text-primary mb-4">
+            pricing
+          </p>
+          <h2 className="text-3xl lg:text-4xl font-extrabold mb-12 text-center">
+            A Price that adapts to your needs
+          </h2>
+          <div className="p-8 bg-base-100 max-w-96 rounded-3xl mx-auto space-y-6">
+            <div className="flex gap-2 items-baseline justify-center">
+              <div className="text-4xl font-black">$19</div>
+              <div className="uppercase font-md opacity-69 text-sm">/month</div>
+            </div>{" "}
+            <ul className="space-y-2">
+              <li className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="text-green-400 size-4"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                Collect Customer Feedback
+              </li>
+              <li className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="text-green-400 size-4"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                Unlimited Boards
+              </li>
+              <li className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="text-green-400 size-4"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                Admin Dashboard
+              </li>
+              <li className="flex gap-2 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="text-green-400 size-4"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                24/7 Support
+              </li>
+            </ul>
+            <ButtonLogin isLoggedIn={isLoggedIn} name={name} extraStyle="w-full" />
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
+
+// SVG Scalable Vector Graphic (group of vectors of how to create image SVG is much smoother)
+// SVG is much faster - tons of svg libraries
--- components/ButtonLogin.js
+++ components/ButtonLogin.js
@@ -1,13 +1,20 @@
 import Link from "next/link";
 
-const ButtonLogin = ({isLoggedIn, name}) => {
-  if(isLoggedIn) {
-    return <Link className="btn btn-primary" href="/dashboard">Welcome back {name} 
-    
-    {/* {children} */}
-    </Link>;
+const ButtonLogin = ({ isLoggedIn, name, extraStyle }) => {
+  console.log("ExtraStyle", extraStyle)
+  if (isLoggedIn) {
+    return (
+      <Link
+      // using conditional and ternary to make button look different based on props passed
+        className={`btn btn-primary ${extraStyle ? extraStyle : ""}`}
+        href="/dashboard"
+      >
+        Welcome Back {name}
+        {/* {children} */}
+      </Link>
+    );
   } else {
-    return <button className="btn0">Log In</button>
+    return <button className="btn0">Log In</button>;
   }
 };
 
