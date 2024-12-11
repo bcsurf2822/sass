9cad76e93075006097ac314463c5a7fe7ce1c019
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-12-11 16:13:09 -0500

    pase on api section error

--- components/FormNewBoard.js
+++ components/FormNewBoard.js
@@ -1,24 +1,62 @@
-import React from "react";
+"use client";
 
-export default function FormNewBoard() {
+import { useState } from "react";
+
+const FormNewBoard = () => {
+  const [name, setName] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (isLoading) return;
+
+    setIsLoading(true);
+    try {
+      await fetch("/api/board", {
+        method: "POST",
+        body: JSON.stringify({
+          name,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    } catch (error) {
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <div className="bg-base-100 p-8 rounded-3xl space-y-8">
+    <form
+      className="bg-base-100 p-8 rounded-3xl space-y-8"
+      onSubmit={handleSubmit}
+    >
       {/* TITLE */}
       <p className="font-bold text-lg">Create a new feedback board</p>
       {/* FORM */}
       <label className="form-control w-full ">
         <div className="label">
           <span className="label-text font-bold">Board Name</span>
-
         </div>
         <input
+          required
           type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
           placeholder="Type here"
           className="input input-bordered w-full"
         />
       </label>
       {/* BUTTON */}
-      <button className="btn btn-primary btn-block">Create Board</button>
-    </div>
+      <button type="submit" className="btn btn-primary btn-block">
+        {isLoading && (
+          <span className="loading loading-spinner loading-xs"></span>
+        )}
+        Create Board
+      </button>
+    </form>
   );
-}
+};
+
+export default FormNewBoard;
